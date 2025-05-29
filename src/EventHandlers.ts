63d1571907523eab81b1012c2007017050a46add
parizval <anmolgoyal@gmail.com>
2025-05-29 11:23:48 +0530

    added token deposit and output params in the OrderInventory

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -37,6 +37,8 @@ VaultFactory.AssetDeposited.handlerWithLoader({
     if (order) {
       const existingOrder: VaultFactory_OrderInventory = {
         ...order,
+        depositToken: event.params.depositToken,
+        convertToken: event.params.convertToken,
         status: 1,
       };
       context.VaultFactory_OrderInventory.set(existingOrder);
@@ -151,6 +153,8 @@ VaultFactory.OrderCreated.handler(async ({ event, context }) => {
     conditionValue: event.params.conditionValue,
     vault: event.params.vault,
     orderId: event.params.orderId,
+    depositToken: "",
+    convertToken: "",
     status: 0,
     solverTransaction: "",
   };
